eebe18b64a40482cce0e7b15c8d9ee48cffb4753
yiyunlau-adroit <yiyunlau@gmail.com>
2026-04-30 12:13:31 +0800

    Add serial Blink message during LED cycle

--- blink.ino
+++ blink.ino
@@ -3,10 +3,12 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Blink!");
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
